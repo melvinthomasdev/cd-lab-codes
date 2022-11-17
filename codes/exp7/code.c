d8cbdc035af4406cf136748fccc37766da70b3bd
= <=>
2022-11-17 13:39:23 +0530

    exp 7 and 8 ip

--- codes/exp7/code.c
+++ codes/exp7/code.c
@@ -1,36 +1,81 @@
 #include <stdio.h>
 #include <string.h>
-
+#include<stdlib.h>
 void main()
 {
     char code[10][30], str[20], opr[10];
-    int i = 0;
-    printf("\n Enter the set of intermediate code (terminated by exit):\n");
-    do {
-        scanf("%s", code[i]);
-    } while (strcmp(code[i++], "exit") != 0);
-    printf("\nTarget Code Generation");
-    i = 0;
-    do {
-        strcpy(str, code[i]);
-        switch (str[3]){
+    int i = 0,k=0;
+    FILE *fp1,*fp2;
+    fp1=fopen("input.txt","r");
+    //printf("FILE OPENED\n");
+    fp2=fopen("output.txt","w+");
+    char op,fir[10],sec[10],res[10];
+    while(!feof(fp1)){
+
+        fscanf(fp1,"%c\t%s\t%s\t%s\n",&op,fir,sec,res);
+        //printf("%c\t%s\t%s\t%s\n",op,fir,sec,res);
+    
+        switch (op){
                 case '+':
                         strcpy(opr, "ADD ");
+                        i=0;
                         break;
                 case '-':
                         strcpy(opr, "SUB");
+                        i=0;
                         break;
                 case '*':
                         strcpy(opr, "MUL ");
+                        i=1;
                         break;
                 case '/' :
                         strcpy(opr, "DIV");
+                        i=1;
                         break;
             }
-            printf("\n\tMOV %c,R%d", str[2], i);
-            printf("\n\t%s%c,R%d", opr, str[4], i);
-            printf("\n\tMOV R%d,%c", i, str[0]);
-        } while (strcmp(code[++i], "exit") != 0);
-
-        printf("\n");
+            if(strlen(fir)==2)
+            {
+                if (fir[1]=='1')
+                        fprintf(fp2,"MOV AX,DH\n");
+                else if (fir[1]=='2')
+                        fprintf(fp2,"MOV BX,DL\n");
+                else if (fir[1]=='3')
+                        fprintf(fp2,"MOV CX,CH\n");
+                else if (fir[1]=='4')
+                        fprintf(fp2,"MOV DX,CL\n");
+            }
+            else
+            fprintf(fp2,"MOV AX,[%s]\n", fir);
+            if(strlen(sec)==2)
+            {
+                if (sec[1]=='1')
+                        fprintf(fp2,"MOV BX,DH\n");
+                else if (sec[1]=='2')
+                        fprintf(fp2,"MOV BX,DL\n");
+                else if (sec[1]=='3')
+                        fprintf(fp2,"MOV BX,CH\n");
+                else if (sec[1]=='4')
+                        fprintf(fp2,"MOV BX,CL\n");
+            }
+            else
+                fprintf(fp2,"MOV BX,[%s]\n",sec);
+            if (i==0)
+            fprintf(fp2,"%sAX,BX\n", opr);
+            else
+            fprintf(fp2,"%sBX\n", opr);
+            if(strlen(res)==2)
+            {
+                if (res[1]=='1')
+                        fprintf(fp2,"MOV DH,AX\n");
+                else if (res[1]=='2')
+                        fprintf(fp2,"MOV DL,AX\n");
+                else if (res[1]=='3')
+                        fprintf(fp2,"MOV CH,AX\n");
+                else if (res[1]=='4')   
+                        fprintf(fp2,"MOV CL,AX\n");
+            }
+            
+            k++;
+            
+    }
 }
